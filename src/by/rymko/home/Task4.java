54545c0c4e7fdc1a68ce8ee2fe3679500e3e7ecc
Alexander Rymko <RymkoAlex@gmail.com>
2021-09-16 15:41:24 +0300

    Some message

--- src/by/rymko/home/Task4.java
+++ src/by/rymko/home/Task4.java
@@ -9,7 +9,7 @@ public class Task4 {
         int i, j;
         for (i = 0; i < array1.length; i++) {
             for (j = 0; j < array1[i].length; j++) {
-                if (i == j) {
+                if (i == j||i+j== (array1[i].length-1)) {
                     array1[i][j] = 1;
                 }
                 System.out.print(array1[i][j] + "  ");
